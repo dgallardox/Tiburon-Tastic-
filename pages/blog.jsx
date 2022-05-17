62c371ec30cd26f5152061fdfa9d17460cef89cc
Diego Gallardo <diego.gallardo@wpengine.com>
2022-05-17 14:03:29 -0500

    test

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -16,7 +16,7 @@ export default function blog({ posts }) {
           <Card>
           <div id="postsDiv">
                 <h4>{post.title}</h4>
-                <p>hello 234</p>
+                <p>hello 234d</p>
             <p dangerouslySetInnerHTML={{ __html: post.content }}></p>
               </div>
             </Card>
